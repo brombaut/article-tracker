722c2c0830c7ba137c83a17e039814781586caf4
Ben Rombaut <rombaut.benj@gmail.com>
2020-06-22 09:24:24 -0300

    Update scraper to work with dev.to site updates

--- src/components/AddArticleForm.vue
+++ src/components/AddArticleForm.vue
@@ -1,50 +1,33 @@
 <template>
-    <form id='add-article-form' autocomplete="off">
-        <h2>New Article Record</h2>
-        <p>For dev.to articles, just enter the URL, and I'll scrape the article's 'Title' and 'Minute Read' fields.</p>
-        <div class='form-element'>
-            <label for="article-url">URL:</label>
-            <input
-                type="text"
-                id="article-url"
-                name="article-url"
-                v-model="articleUrl">
-        </div>
-        <div class='form-element'>
-            <label for="article-title">Title:</label>
-            <input
-                type="text"
-                id="article-title"
-                name="article-title"
-                v-model="articleTitle">
-        </div>
-        <div class='form-element'>
-            <div id='add-article-read-container'>
-                <label for="has-been-read">
-                    <input
-                        type="checkbox"
-                        id="has-been-read"
-                        name="has-been-read"
-                        v-model="articleRead">
-                    <span>Read</span>
-                </label>
-            </div>
-        </div>
-        <div id='add-article-submit-container'>
-            <button
-                id='add-article-submit-button'
-                @click="handleSubmit">
-                <div
-                    class="lds-dual-ring"
-                    v-if="isSubmitting">
-                </div>
-                <span v-else>Submit</span>
-            </button>
-        </div>
-        <div v-if="showOnlyBenMessage" id='only-ben-message'>
-            <span>Only Ben can add articles to his reading list</span>
-        </div>
-    </form>
+  <form id="add-article-form" autocomplete="off">
+    <h2>New Article Record</h2>
+    <p>For dev.to articles, just enter the URL, and I'll scrape the article's 'Title' and 'Minute Read' fields.</p>
+    <div class="form-element">
+      <label for="article-url">URL:</label>
+      <input type="text" id="article-url" name="article-url" v-model="articleUrl" />
+    </div>
+    <div class="form-element">
+      <label for="article-title">Title:</label>
+      <input type="text" id="article-title" name="article-title" v-model="articleTitle" />
+    </div>
+    <div class="form-element">
+      <div id="add-article-read-container">
+        <label for="has-been-read">
+          <input type="checkbox" id="has-been-read" name="has-been-read" v-model="articleRead" />
+          <span>Read</span>
+        </label>
+      </div>
+    </div>
+    <div id="add-article-submit-container">
+      <button id="add-article-submit-button" @click="handleSubmit">
+        <div class="lds-dual-ring" v-if="isSubmitting"></div>
+        <span v-else>Submit</span>
+      </button>
+    </div>
+    <div v-if="showOnlyBenMessage" id="only-ben-message">
+      <span>Only Ben can add articles to his reading list</span>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -52,237 +35,235 @@ import { bus } from '@/main';
 import scraper from '@/services/scraper';
 
 export default {
-    name: 'AddArticleForm',
-    data() {
-        return {
-            articleTitle: '',
-            articleUrl: '',
-            articleRead: false,
-            isSubmitting: false,
-            showOnlyBenMessage: false,
-        };
-    },
-    methods: {
-        handleSubmit(e) {
-            e.preventDefault();
-            this.setIsSubmitting();
-            let stop = false;
-            stop = this.validateUrl();
-            if (stop) {
-                this.setIsNotSubmitting();
-                return;
-            }
-            if (!this.articleTitle) {
-                this.attemptArticleInformationScrape();
-                return;
-            }
+  name: 'AddArticleForm',
+  data() {
+    return {
+      articleTitle: '',
+      articleUrl: '',
+      articleRead: false,
+      isSubmitting: false,
+      showOnlyBenMessage: false,
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.setIsSubmitting();
+      let stop = false;
+      stop = this.validateUrl();
+      if (stop) {
+        this.setIsNotSubmitting();
+        return;
+      }
+      if (!this.articleTitle) {
+        this.attemptArticleInformationScrape();
+        return;
+      }
 
-            stop = this.validateTitle();
-            if (stop) {
-                this.setIsNotSubmitting();
-                return;
-            }
-            this.setIsSubmitting();
-            const article = {
-                title: this.articleTitle,
-                url: this.articleUrl,
-                read: this.articleRead,
-                minuteRead: null,
-                tags: [],
-            };
-            bus.$emit('addArticleFormSubmitted', article);
-        },
-        attemptArticleInformationScrape() {
-            scraper.scrapeArticleInformation(this.articleUrl).then(scrapedArticleInfo => {
-                this.articleTitle = scrapedArticleInfo.articleTitle;
-                this.validateTitle();
-                if (!scrapedArticleInfo.articleTitle) {
-                    this.setIsNotSubmitting();
-                    return;
-                }
-                const article = {
-                    title: this.articleTitle,
-                    url: this.articleUrl,
-                    read: this.articleRead,
-                    minuteRead: scrapedArticleInfo.minuteRead,
-                    tags: scrapedArticleInfo.tags,
-                };
-                bus.$emit('addArticleFormSubmitted', article);
-            });
-        },
-        validateUrl() {
-            const urlInput = document.querySelector('#article-url');
-            if (!this.articleUrl) {
-                urlInput.classList.add('red-border');
-                return true;
-            }
-            urlInput.classList.remove('red-border');
-            return false;
-        },
-        validateTitle() {
-            const titleInput = document.querySelector('#article-title');
-            if (!this.articleTitle) {
-                titleInput.classList.add('red-border');
-                return true;
-            }
-            titleInput.classList.remove('red-border');
-            return false;
-        },
-        setIsSubmitting() {
-            this.isSubmitting = true;
-            document.querySelector('#article-title').disabled = true;
-            document.querySelector('#article-url').disabled = true;
-            document.querySelector('#has-been-read').disabled = true;
-            document.querySelector('#add-article-submit-button').disabled = true;
-        },
-        setIsNotSubmitting() {
-            this.isSubmitting = false;
-            document.querySelector('#article-title').disabled = false;
-            document.querySelector('#article-url').disabled = false;
-            document.querySelector('#has-been-read').disabled = false;
-            document.querySelector('#add-article-submit-button').disabled = false;
-        },
-        resetForm() {
-            this.isSubmitting = false;
-            this.articleTitle = '';
-            this.articleUrl = '';
-            this.articleRead = false;
-            document.querySelector('#article-title').disabled = false;
-            document.querySelector('#article-url').disabled = false;
-            document.querySelector('#has-been-read').disabled = false;
-            document.querySelector('#add-article-submit-button').disabled = false;
-        },
-        setShowOnlyBenMessage(val) {
-            this.showOnlyBenMessage = val;
-        },
-        handleNotBenError() {
-            this.setShowOnlyBenMessage(true);
-            this.isSubmitting = false;
-            document.querySelector('#article-title').disabled = false;
-            document.querySelector('#article-url').disabled = false;
-            document.querySelector('#has-been-read').disabled = false;
-            document.querySelector('#add-article-submit-button').disabled = false;
-            setTimeout(() => this.setShowOnlyBenMessage(false), 5000);
-        },
+      stop = this.validateTitle();
+      if (stop) {
+        this.setIsNotSubmitting();
+        return;
+      }
+      this.setIsSubmitting();
+      const article = {
+        title: this.articleTitle,
+        url: this.articleUrl,
+        read: this.articleRead,
+        minuteRead: null,
+        tags: [],
+      };
+      bus.$emit('addArticleFormSubmitted', article);
     },
-    mounted() {
-        bus.$on('clearArticleForm', this.resetForm);
-        bus.$on('addNewArticleError', this.handleNotBenError);
+    attemptArticleInformationScrape() {
+      scraper.scrapeArticleInformation(this.articleUrl).then(scrapedArticleInfo => {
+        this.articleTitle = scrapedArticleInfo.articleTitle;
+        this.validateTitle();
+        if (!scrapedArticleInfo.articleTitle) {
+          this.setIsNotSubmitting();
+          return;
+        }
+        const article = {
+          title: this.articleTitle,
+          url: this.articleUrl,
+          read: this.articleRead,
+          minuteRead: scrapedArticleInfo.minuteRead,
+          tags: scrapedArticleInfo.tags,
+        };
+        bus.$emit('addArticleFormSubmitted', article);
+      });
     },
+    validateUrl() {
+      const urlInput = document.querySelector('#article-url');
+      if (!this.articleUrl) {
+        urlInput.classList.add('red-border');
+        return true;
+      }
+      urlInput.classList.remove('red-border');
+      return false;
+    },
+    validateTitle() {
+      const titleInput = document.querySelector('#article-title');
+      if (!this.articleTitle) {
+        titleInput.classList.add('red-border');
+        return true;
+      }
+      titleInput.classList.remove('red-border');
+      return false;
+    },
+    setIsSubmitting() {
+      this.isSubmitting = true;
+      document.querySelector('#article-title').disabled = true;
+      document.querySelector('#article-url').disabled = true;
+      document.querySelector('#has-been-read').disabled = true;
+      document.querySelector('#add-article-submit-button').disabled = true;
+    },
+    setIsNotSubmitting() {
+      this.isSubmitting = false;
+      document.querySelector('#article-title').disabled = false;
+      document.querySelector('#article-url').disabled = false;
+      document.querySelector('#has-been-read').disabled = false;
+      document.querySelector('#add-article-submit-button').disabled = false;
+    },
+    resetForm() {
+      this.isSubmitting = false;
+      this.articleTitle = '';
+      this.articleUrl = '';
+      this.articleRead = false;
+      document.querySelector('#article-title').disabled = false;
+      document.querySelector('#article-url').disabled = false;
+      document.querySelector('#has-been-read').disabled = false;
+      document.querySelector('#add-article-submit-button').disabled = false;
+    },
+    setShowOnlyBenMessage(val) {
+      this.showOnlyBenMessage = val;
+    },
+    handleNotBenError() {
+      this.setShowOnlyBenMessage(true);
+      this.isSubmitting = false;
+      document.querySelector('#article-title').disabled = false;
+      document.querySelector('#article-url').disabled = false;
+      document.querySelector('#has-been-read').disabled = false;
+      document.querySelector('#add-article-submit-button').disabled = false;
+      setTimeout(() => this.setShowOnlyBenMessage(false), 5000);
+    },
+  },
+  mounted() {
+    bus.$on('clearArticleForm', this.resetForm);
+    bus.$on('addNewArticleError', this.handleNotBenError);
+  },
 };
 </script>
 
 <style lang='scss'>
 #add-article-form {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  .form-element {
+    margin: 20px 0;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+    width: 100%;
 
-    .form-element {
-        margin: 20px 0;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        width: 100%;
-
-        label {
-            font-size: 24px;
-        }
-
-        input[type=text] {
-            padding: 6px;
-            border: 2px solid $primary;
-            border-radius: 4px;
-            width: calc(100% - 8px);
-            font-size: 20px;
-
-            &:focus {
-                border: 2px solid $primary;
-            }
-        }
-
-        .red-border {
-            border: 2px solid #b94242 !important;
-        }
-
-        #add-article-read-container {
-            margin: 4px;
-            background-color: #EFEFEF;
-            border-radius: 4px;
-            border: 1px solid #D0D0D0;
-            transition: 0.3s;
-
-            &:hover {
-                cursor: pointer;
-                background: #a0ffd4;
-            }
-
-            label {
-                &:hover {
-                    cursor: pointer;
-                }
-
-                span {
-                    text-align: center;
-                    padding: 12px 18px;;
-                    display: block;
-                    transition: 0.3s;
-                }
-
-                input {
-                    display: none
-                }
-            }
-
-
-            input:checked + span {
-                background-color: $primary;
-                color: #fff;
-            }
-        }
+    label {
+      font-size: 24px;
     }
 
-    #add-article-submit-container {
-        align-self: flex-end;
+    input[type="text"] {
+      padding: 6px;
+      border: 2px solid $primary;
+      border-radius: 4px;
+      width: calc(100% - 8px);
+      font-size: 20px;
 
-        button {
-            padding: 8px;
-            border-radius: 4px;
-
-            &:hover {
-                cursor: pointer;
-                background: $primaryBrighter;
-            }
-
-            span {
-                padding: 7px 0;
-            }
-
-            .lds-dual-ring {
-                display: inline-block;
-            }
-
-            .lds-dual-ring:after {
-                content: " ";
-                display: block;
-                width: 20px;
-                height: 20px;
-                margin: 8px;
-                border-radius: 50%;
-                border: 3px solid $secondary;
-                border-color: $secondary transparent $secondary transparent;
-                animation: lds-dual-ring 1.2s linear infinite;
-            }
-            @keyframes lds-dual-ring {
-                0% {
-                    transform: rotate(0deg);
-                }
-                100% {
-                    transform: rotate(360deg);
-                }
-            }
-        }
+      &:focus {
+        border: 2px solid $primary;
+      }
     }
+
+    .red-border {
+      border: 2px solid #b94242 !important;
+    }
+
+    #add-article-read-container {
+      margin: 4px;
+      background-color: #efefef;
+      border-radius: 4px;
+      border: 1px solid #d0d0d0;
+      transition: 0.3s;
+
+      &:hover {
+        cursor: pointer;
+        background: #a0ffd4;
+      }
+
+      label {
+        &:hover {
+          cursor: pointer;
+        }
+
+        span {
+          text-align: center;
+          padding: 12px 18px;
+          display: block;
+          transition: 0.3s;
+        }
+
+        input {
+          display: none;
+        }
+      }
+
+      input:checked + span {
+        background-color: $primary;
+        color: #fff;
+      }
+    }
+  }
+
+  #add-article-submit-container {
+    align-self: flex-end;
+
+    button {
+      padding: 8px;
+      border-radius: 4px;
+
+      &:hover {
+        cursor: pointer;
+        background: $primaryBrighter;
+      }
+
+      span {
+        padding: 7px 0;
+      }
+
+      .lds-dual-ring {
+        display: inline-block;
+      }
+
+      .lds-dual-ring:after {
+        content: " ";
+        display: block;
+        width: 20px;
+        height: 20px;
+        margin: 8px;
+        border-radius: 50%;
+        border: 3px solid $secondary;
+        border-color: $secondary transparent $secondary transparent;
+        animation: lds-dual-ring 1.2s linear infinite;
+      }
+      @keyframes lds-dual-ring {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+    }
+  }
 }
-
 </style>
--- src/services/scraper.js
+++ src/services/scraper.js
@@ -6,93 +6,93 @@ const devToBaseUrl = 'https://dev.to';
 const corsPrefix = 'https://cors-anywhere.herokuapp.com/';
 
 function buildArticleInformationFromHtml(html) {
-    const $ = buildHtmlTree(html);
-    const mainTitleNode = selectMainTitleNode($);
-    const articleTitle = scrapeArticleTItle($, mainTitleNode);
-    const minuteRead = scrapeMinuteRead($, mainTitleNode);
-    const tags = scrapeTags($, mainTitleNode);
-    return Promise.resolve({
-        articleTitle,
-        minuteRead,
-        tags,
-    });
+  const $ = buildHtmlTree(html);
+  const mainTitleNode = selectMainTitleNode($);
+  const articleTitle = scrapeArticleTItle($, mainTitleNode);
+  const minuteRead = scrapeMinuteRead($, mainTitleNode);
+  const tags = scrapeTags($, mainTitleNode);
+  return Promise.resolve({
+    articleTitle,
+    minuteRead,
+    tags,
+  });
 }
 
 function buildHtmlTree(htmlString) {
-    return cheerio.load(htmlString);
+  return cheerio.load(htmlString);
 }
 
 function selectMainTitleNode($) {
-    return $('#main-title');
+  return $('#main-title');
 }
 
 function scrapeArticleTItle($, mainTitleNode) {
-    let articleTitle = null;
-    try {
-        articleTitle = $(mainTitleNode).find('h1').text().trim();
-    } catch {
-        console.error('Could not parse article title string');
-    }
-    return articleTitle;
+  let articleTitle = null;
+  try {
+    articleTitle = $(mainTitleNode).find('h1').text().trim();
+  } catch {
+    console.error('Could not parse article title string');
+  }
+  return articleTitle;
 }
 
 function scrapeMinuteRead($, mainTitleNode) {
-    let minuteRead = null;
-    try {
-        const minuteReadString = $(mainTitleNode).find('h3').find('.published-at').text();
-        [minuteRead] = minuteReadString.match(/\d+/g).map(Number);
-    } catch {
-        console.error('Could not parse minute read string');
-    }
-    return minuteRead;
+  let minuteRead = null;
+  try {
+    const minuteReadString = $(mainTitleNode).find('time').next().text(); // Get next sibling
+    [minuteRead] = minuteReadString.match(/\d+/g).map(Number);
+  } catch {
+    console.error('Could not parse minute read string');
+  }
+  return minuteRead;
 }
 
 function scrapeTags($, mainTitleNode) {
-    const tags = [];
-    try {
-        const tagNodes = $(mainTitleNode).find('.tags').find('a');
-        tagNodes.each((i, node) => {
-            const name = $(node).text();
-            const href = `${devToBaseUrl}${$(node).attr('href')}`;
-            const stylesString = $(node).attr('style');
-            const stylesArray = stylesString.split(';');
-            let color = null;
-            let backgroundColor = null;
-            stylesArray.forEach(styleString => {
-                const [name, value] = styleString.split(':');
-                if (name === 'color') {
-                    color = value;
-                }
-                if (name === 'background-color') {
-                    backgroundColor = value;
-                }
-            });
-            const tagObject = {
-                name,
-                href,
-                color,
-                backgroundColor,
-            };
-            tags.push(tagObject);
-        });
-    } catch {
-        console.error('Could not scrape tags data');
-    }
-    return tags;
+  const tags = [];
+  try {
+    const tagNodes = $(mainTitleNode).find('.spec__tags').find('a');
+    tagNodes.each((i, node) => {
+      const name = $(node).text();
+      const href = `${devToBaseUrl}${$(node).attr('href')}`;
+      const stylesString = $(node).attr('style');
+      const stylesArray = stylesString.split(';');
+      let color = null;
+      let backgroundColor = null;
+      stylesArray.forEach(styleString => {
+        const [name, value] = styleString.split(':');
+        if (name === 'color') {
+          color = value;
+        }
+        if (name === 'background-color') {
+          backgroundColor = value;
+        }
+      });
+      const tagObject = {
+        name,
+        href,
+        color,
+        backgroundColor,
+      };
+      tags.push(tagObject);
+    });
+  } catch {
+    console.error('Could not scrape tags data');
+  }
+  return tags;
 }
 
 export default {
-    scrapeArticleInformation(articleUrl) {
-        const url = `${corsPrefix}${articleUrl}`;
-        return axios.get(url).then(response => {
-            if (response.status !== 200) {
-                const rejectObject = {
-                    code: response.status,
-                    message: 'Could not get article from url',
-                };
-                return Promise.reject(rejectObject);
-            }
-            return buildArticleInformationFromHtml(response.data);
-        });
-    },
+  scrapeArticleInformation(articleUrl) {
+    const url = `${corsPrefix}${articleUrl}`;
+    return axios.get(url).then(response => {
+      if (response.status !== 200) {
+        const rejectObject = {
+          code: response.status,
+          message: 'Could not get article from url',
+        };
+        return Promise.reject(rejectObject);
+      }
+      return buildArticleInformationFromHtml(response.data);
+    });
+  },
 };
